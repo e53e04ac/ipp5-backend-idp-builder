1b2ac9c0a3231c549bf4f10bb44d195155265acd
@e53e04ac <e53e04ac@gmail.com>
2023-02-25 17:08:48 +0900

    0

--- index.mjs
+++ index.mjs
@@ -67,7 +67,7 @@ const constructor = ((options) => {
             return _self.scriptDirectory().resolve('Dockerfile.template');
         }),
         runDirectory: hold(() => {
-            return _options.tmpDirectory().resolve('ipp5-backend-idp-builder', _options.runId());
+            return _options.tmpDirectory().resolve(_options.runId());
         }),
         contextDirectory: hold(() => {
             return _self.runDirectory().resolve('context');
